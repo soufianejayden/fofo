d7efda6d5d3e763832f34c7516e8795f530bbe1f
soufianejayden <82966385+soufianejayden@users.noreply.github.com>
2022-08-12 22:56:10 +0100

    first commit

---
+++ src/formSource.js
@@ -0,0 +1,118 @@
+export const userInputs =[
+
+    {
+        id:1,
+        label:"Username",
+        type:"text",
+        placeholder:"john",
+    },
+
+    {
+        id:2,
+        label:"Name and surname",
+        type:"text",
+        placeholder:"john_doe",
+    },
+
+    {
+        id:3,
+        label:"Email",
+        type:"mail",
+        placeholder:"john_soso@gmail.com",
+    },
+    {
+        id:4,
+        label:"phone",
+        type:"text",
+        placeholder:"+1 2216 354 22",
+    },
+    {
+        id:5,
+        label:"password",
+        type:"password",
+     },
+
+    {
+        id:6,
+        label:"Address",
+        type:"text",
+        placeholder:"Elton st. 216 NewYork",
+    },
+
+    {
+        id:7,
+        label:"Country",
+        type:"text",
+        placeholder:"USA",
+    },
+
+];
+
+    export const productInputs = [
+        {
+          id: 1,
+          label: "Title",
+          type: "text",
+          placeholder: "Apple Macbook Pro",
+        },
+        {
+          id: 2,
+          label: "Description",
+          type: "text",
+          placeholder: "Description",
+        },
+        {
+          id: 3,
+          label: "Category",
+          type: "text",
+          placeholder: "Computers",
+        },
+        {
+          id: 4,
+          label: "Price",
+          type: "text",
+          placeholder: "100",
+        },
+        {
+          id: 5,
+          label: "Stock",
+          type: "text",
+          placeholder: "in stock",
+        },
+      ];
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
